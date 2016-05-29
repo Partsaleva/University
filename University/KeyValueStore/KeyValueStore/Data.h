a8498d79c6b0c61c04f7cb61084177c728f6f8b6
Partsaleva <Partsaleva@users.noreply.github.com>
2016-05-29 21:36:13 +0300

    delete uni projects

--- University/KeyValueStore/KeyValueStore/Data.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-struct Data {
-
-	Data():key(),memoryBlock(){}
-	Data(size_t dataKey, std::string data) :key(dataKey), memoryBlock(data),next(nullptr){	}
-	std::string memoryBlock;
-	size_t key;
-	Data* next;
-};
